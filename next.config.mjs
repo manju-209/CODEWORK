8c514963362a1bbdc847e11f6cd21cd897e3ae43
Manju <manjukrishna.m@codework.ai>
2026-01-09 12:26:05 +0530

    Update config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     unoptimized: true,
   },
-  
+  assetPrefix: "",
+  trailingSlash: true,
 };
+
 export default nextConfig;
+
